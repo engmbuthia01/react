a071bf3dabf2d971df0215ab80afd3cf96198261
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-09 04:19:45 -0400

    created Table custom components

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -3,28 +3,9 @@ import Table from './Table'
 
 class App extends Component {
     render() {
-        const characters = [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspiring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ]
-
         return (
             <div className="container">
-                <Table characterData={characters} />
+                <Table />
             </div>
         )
     }
