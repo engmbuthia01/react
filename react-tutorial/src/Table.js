295d02666ba8b552ee33eadf4f42dfeff51fada2
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-11 00:12:28 -0400

    used props to load data into the rows of our table

--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -11,13 +11,21 @@ const TableHeader = () => {
     )
 }
 
-const TableBody = () => {
-    return <tbody />
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
 }
 
 class Table extends Component {
     render() {
-        const { characterData } = this.props;
+        const { characterData } = this.props
         return (
             <table>
                 <TableHeader />
