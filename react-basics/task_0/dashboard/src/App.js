7c755a596eb9e635a25e1f83d775b28269f251e2
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-09-06 11:16:29 -0400

    created a react app

---
+++ react-basics/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./alx-logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App-header">
+        <img src="alx-logo.jpg" alt="This is a logo for ALX Africa"></img>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - ALX</p>
+      </div>
+    </React.Fragment>
+  );
+}
